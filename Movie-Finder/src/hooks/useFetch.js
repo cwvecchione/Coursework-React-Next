b4a5f511100b7cc2a39a2d4d10ef69de9f7e33bc
Chris Vecchione <cwvecchione@gmail.com>
2025-12-17 10:59:34 -0500

    Initial commit

---
+++ Movie-Finder/src/hooks/useFetch.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = (apiPath, queryTerm="") => {
+    const [data, setData] = useState([]);
+        
+    const url = `https://api.themoviedb.org/3/${apiPath}?language=en-US&page=1&query=${queryTerm}`
+
+    useEffect(() => {
+        async function fetchMovies(){
+            const options = {
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                    Authorization: `Bearer ${process.env.TMDB_READ_ACCESS_TOKEN}`
+                }
+            };
+            const response = await fetch(url, options);
+            const json = await response.json();
+            setData(json.results);
+        }
+        fetchMovies();
+    }, [url])
+
+    return { data }
+}
