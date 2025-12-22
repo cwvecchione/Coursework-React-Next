9064b9805aa1fd12f813150891f8cc339abf1369
Chris Vecchione <cwvecchione@gmail.com>
2025-12-22 12:17:49 -0500

    env updates

--- Movie-Finder/src/hooks/useFetch.js
+++ Movie-Finder/src/hooks/useFetch.js
@@ -3,15 +3,15 @@ import { useState, useEffect } from "react";
 export const useFetch = (apiPath, queryTerm="") => {
     const [data, setData] = useState([]);
         
-    const url = `https://api.themoviedb.org/3/${apiPath}?language=en-US&page=1&query=${queryTerm}`
+    const url = `https://api.themoviedb.org/3/${apiPath}?language=en-US&page=1${queryTerm ? `&query=${queryTerm}` : ""}`;
 
     useEffect(() => {
         async function fetchMovies(){
             const options = {
                 method: 'GET',
                 headers: {
-                    accept: 'application/json',
-                    Authorization: `Bearer ${process.env.TMDB_READ_ACCESS_TOKEN}`
+                    Accept: 'application/json',
+                    Authorization: `Bearer ${process.env.REACT_TMDB_READ_ACCESS_TOKEN}`
                 }
             };
             const response = await fetch(url, options);
